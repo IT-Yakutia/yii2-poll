df22159fd162f512a5b76b89e97fb1a85a235d1f
PlayerZeroOne <general.dmf@gmail.com>
2020-04-24 00:29:49 +0300

    cookie added

--- src/controllers/FrontController.php
+++ src/controllers/FrontController.php
@@ -5,8 +5,10 @@ namespace ityakutia\poll\controllers;
 use ityakutia\poll\models\Poll;
 use ityakutia\poll\models\PollSearch;
 use ityakutia\poll\models\PollVote;
+// use Symfony\Component\BrowserKit\Cookie;
 use Yii;
 use yii\web\Controller;
+use yii\web\Cookie;
 use yii\web\NotFoundHttpException;
 
 class FrontController extends Controller
@@ -28,7 +30,15 @@ class FrontController extends Controller
         $model = $this->findModel($id);
 
         $answer = Yii::$app->request->get()['answer'];
+        $is_voted = Yii::$app->request->cookies->getValue("poll_voted_$id", false);
 
+        if($is_voted) {
+            // var_dump($is_voted);
+            return $this->render('thanks', [
+                'model' => $model,
+                'is_voted' => true
+            ]);
+        }
 
         if (!empty($answer)) {
             foreach ($answer as $poll_id => $questions) {
@@ -45,8 +55,12 @@ class FrontController extends Controller
                     }
                 }
 
+                $date = strtotime(date('Y-m-d H:i:s', strtotime('+1 month')));
+                Yii::$app->response->cookies->add(new Cookie(['name' => "poll_voted_$id", 'value' => 1,'expire' => $date]));
+
                 return $this->render('thanks', [
-                    'model' => $model
+                    'model' => $model,
+                    'is_voted' => false
                 ]);
             }
         }
@@ -65,7 +79,3 @@ class FrontController extends Controller
         throw new NotFoundHttpException('The requested page does not exist.');
     }
 }
-
-// написать контроллер, представления примерно сделаны, в случае необходимости дополнить представления
-
-// для формы, нужно будет добавить ActiveForm, модель и валидацию
--- src/views/front/thanks.php
+++ src/views/front/thanks.php
@@ -1,10 +1,13 @@
 <?php
 
-$this->title = 'Спасибо что приняли участие в нашем опросе!';
+use yii\helpers\Html;
+
+$this->title = $is_voted ? 'Вы уже проголосовали в данном опросе!' : 'Спасибо что приняли участие в нашем опросе!';
 
 ?>
 
 <div class="poll" data-id="<?= $model->id ?>">
     <h3><?= $this->title ?></h3>
     <p><?= $model->title ?></p>
+    <?= Html::a('Вернуться на страницу опросов', ['index'], ['class' => 'btn']) ?>
 </div>
