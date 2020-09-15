e960b72358ec273079cf2e56d5aa037d8bdbf7f4
playerzero <general.dmf@gmail.com>
2020-09-15 22:30:35 +0300

    links fixed

--- src/views/back/_form.php
+++ src/views/back/_form.php
@@ -24,46 +24,45 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true])->hiddenInput()->label(false) ?>
 
     <?php if (!$model->isNewRecord) { ?>
-    <div class="input-field">
-        <input disabled value="<?= '/'.$model->slug; ?>" id="disabled" type="text" class="validate">
-        <label for="disabled">Относительный url страницы</label>
-    </div>
+        <div class="input-field">
+            <input disabled value="<?= '/poll/front/view?id=' . $model->id ?>" id="disabled" type="text" class="validate">
+            <label for="disabled">Относительный url страницы</label>
+        </div>
 
-    <div class="input-field">
-        <input disabled value="<?= Yii::$app->params['domain'].$model->slug; ?>" id="disabled" type="text" class="validate">
-        <label for="disabled">Асолютный url страницы</label>
-        <?= Html::a("Перейти", '/poll/'. $model->slug, ['target' => "_blank"]); ?>
-    </div>
-    <?php } ?>
+        <div class="input-field">
+            <input disabled value="<?= Yii::$app->params['domain'] . 'poll/front/view?id=' . $model->id ?>" id="disabled" type="text" class="validate">
+            <label for="disabled">Абсолютный url страницы</label>
+            <?= Html::a("Перейти", '/poll/front/view?id=' . $model->id, ['target' => "_blank"]) ?>
+        <?php } ?>
 
-    <?= $form->field($model, 'photo')->widget(Cropper::class, [
-        'aspectRatio' => 350/540,
-        'maxSize' => [540, 350, 'px'],
-        'minSize' => [10, 10, 'px'],
-        'startSize' => [100, 100, '%'],
-        'uploadUrl' => Url::to(['/poll/back/uploadImg']),
-    ]); ?>
-    <small class="grey-text">Загружать изображения размером 540x350 пикселей</small>
+        <?= $form->field($model, 'photo')->widget(Cropper::class, [
+            'aspectRatio' => 350 / 540,
+            'maxSize' => [540, 350, 'px'],
+            'minSize' => [10, 10, 'px'],
+            'startSize' => [100, 100, '%'],
+            'uploadUrl' => Url::to(['/poll/back/uploadImg']),
+        ]); ?>
+        <small class="grey-text">Загружать изображения размером 540x350 пикселей</small>
 
-    <?= $form->field($model, 'meta_description')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'meta_description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'meta_keywords')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'meta_keywords')->textInput(['maxlength' => true]) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton('Сохранить', ['class' => 'btn']) ?>
-    </div>
-    <div class="fixed-action-btn">
-        <?= Html::submitButton('<i class="material-icons">save</i>', [
-            'class' => 'btn-floating btn-large waves-effect waves-light tooltipped',
-            'title' => 'Сохранить',
-            'data-position' => "left",
-            'data-tooltip' => "Сохранить",
-        ]) ?>
-    </div>
+        <div class="form-group">
+            <?= Html::submitButton('Сохранить', ['class' => 'btn']) ?>
+        </div>
+        <div class="fixed-action-btn">
+            <?= Html::submitButton('<i class="material-icons">save</i>', [
+                'class' => 'btn-floating btn-large waves-effect waves-light tooltipped',
+                'title' => 'Сохранить',
+                'data-position' => "left",
+                'data-tooltip' => "Сохранить",
+            ]) ?>
+        </div>
 
-    <?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); ?>
 
-</div>
+        </div>
